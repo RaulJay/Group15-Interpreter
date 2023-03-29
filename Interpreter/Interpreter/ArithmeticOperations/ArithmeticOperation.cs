8a121761413605ae52ed406208e7f4e3dd13670a
Raul Jay Yanson <rjyansonx@gmail.com>
2023-03-29 14:45:32 +0800

    Add arithmetic operations

---
+++ Interpreter/Interpreter/ArithmeticOperations/ArithmeticOperation.cs
@@ -0,0 +1,87 @@
+﻿
+
+namespace Interpreter.ArithmeticOperations
+{
+    public class ArithmeticOperation
+    {
+        public object? Multiply(object x, object y)
+        {
+            if(x is int && y is int)
+                return Multiply(x, y);
+            if(x is float && y is float)
+                return Multiply(x, y);
+            if(x is int && y is float)
+                return Multiply(x, y);
+            if(x is float && y is int)
+                return Multiply(x, y);
+            if (x is string && y is string)
+                return $"{x}{y}";
+
+            return null;
+        }
+
+        public object? Divide(object x, object y)
+        {
+            if (x is int && y is int)
+                return Divide(x, y);
+            if (x is float && y is float)
+                return Divide(x, y);
+            if (x is int && y is float)
+                return Divide(x, y);
+            if (x is float && y is int)
+                return Divide(x, y);
+            if (x is string && y is string)
+                return $"{x}{y}";
+
+            return null;
+        }
+
+        public object? Modulo(object x, object y)
+        {
+            if (x is int && y is int)
+                return Modulo(x, y);
+            if (x is float && y is float)
+                return Modulo(x, y);
+            if (x is int && y is float)
+                return Modulo(x, y);
+            if (x is float && y is int)
+                return Modulo(x, y);
+            if (x is string && y is string)
+                return $"{x}{y}";
+
+            return null;
+        }
+
+        public object? Add(object x, object y)
+        {
+            if (x is int && y is int)
+                return Add(x, y);
+            if (x is float && y is float)
+                return Add(x, y);
+            if (x is int && y is float)
+                return Add(x, y);
+            if (x is float && y is int)
+                return Add(x, y);
+            if (x is string && y is string)
+                return $"{x}{y}";
+
+            return null;
+        }
+
+        public object? Subtract(object x, object y)
+        {
+            if (x is int && y is int)
+                return Subtract(x, y);
+            if (x is float && y is float)
+                return Subtract(x, y);
+            if (x is int && y is float)
+                return Subtract(x, y);
+            if (x is float && y is int)
+                return Subtract(x, y);
+            if (x is string && y is string)
+                return $"{x}{y}";
+
+            return null;
+        }
+    }
+}
--- Interpreter/Interpreter/Grammar/test.ss
+++ Interpreter/Interpreter/Grammar/test.ss
@@ -1,6 +1,7 @@
-﻿# This is a sample CODE
-BEGIN CODE
-    CHAR a_1= 'n'
-    BOOL t = "TRUE"
-    DISPLAY: t a_1
+﻿BEGIN CODE
+    INT x = 5
+    CHAR a = 'n'
+    FLOAT y = 5.5
+    BOOL isTrue = "TRUE"
+    DISPLAY: a
 END CODE
